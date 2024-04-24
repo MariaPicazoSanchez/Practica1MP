fff4533ea7df7297f37d745e4495eeee64ec105a
EstherCarrilero <esther.carrileropolidura@gmail.com>
2024-04-24 07:05:41 +0200

    Commit con prueba en main

--- src/main/java/prac2/Main2.java
+++ src/main/java/prac2/Main2.java
@@ -2,6 +2,10 @@ package prac2;
 
 public class Main2 {
     public static void main(String[] args) {
+        int[] pesos= {1000,3000,4000,5000,7000};
+        practica2 ejemplo = new practica2();
+
+        System.out.println(ejemplo.MochilaMudanza(pesos));
 
     }
 }
--- src/main/java/prac2/practica2.java
+++ src/main/java/prac2/practica2.java
@@ -22,8 +22,8 @@ public class practica2 {
         }
 
         int pos= matriz[0].length-1;
-        for(int k = pesos.length-1; k >= 0; k--){ //recorre filas
-                if (matriz[k][pos]==0) break;
+        for(int k = pesos.length; k >= 0; k--){ //recorre filas
+            if (matriz[k][pos]==0) break;
             if(matriz[k][pos] != matriz[k-1][pos]){
                 solOptima = solOptima + pesos[k-1] + " ";
                 pos = pos - pesos[k-1];
